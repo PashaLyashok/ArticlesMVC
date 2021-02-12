8bfd26074eff4d763f06ac543d85c88795bc9491
PashaLyashok <lyashok_pasha@mail.ru>
2021-02-12 15:10:54 +0300

    'styles falmost finish'

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// https://github.com/PashaLyashok/ArticlesMVC
 // 2.Делать .htaccess (если не получится, то спросить у препода в воскресенье)
 // 3.Делать остальную часть дз
 require_once './app/Autoloader.php';
