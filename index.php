550916bba4000c364c51690d0f55a905a2e91b1f
PashaLyashok <lyashok_pasha@mail.ru>
2021-02-14 12:13:54 +0300

    'qwe'

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 // https://github.com/PashaLyashok/ArticlesMVC
-// 2.Делать .htaccess (если не получится, то спросить у препода в воскресенье)
-// 3.Делать остальную часть дз
+
 require_once './app/Autoloader.php';
 App\DB::connect();
 require_once './app/functions.php';
